534d055f4d80106b57dfcaab7928bc9c19670cb6
niiicolai <niiicolai@gmail.com>
2025-05-28 23:38:19 +0200

    update

--- email_service/src/controllers/middleware/authentication.js
+++ email_service/src/controllers/middleware/authentication.js
@@ -1,16 +1,16 @@
 import JwtService from "../../services/jwt_service.js";
 
-export const authentication = (req, res, next) => {
+export const authentication = async (req, res, next) => {
   const token = req.headers["authorization"]?.split(" ")[1];
   if (!token) return res.status(401).json({ message: "Unauthorized" });
 
-  JwtService.verify(token)
-    .then((decoded) => {
-      req.user = decoded;
-      next();
-    })
-    .catch(() => {
-      res.status(401).json({ message: "Invalid token" });
-    });
+  try {
+    const decoded = JwtService.verify(token);
+    if (!decoded) throw new Error("decoded is undefined");
+    req.user = decoded;
+    next();
+  } catch (e) {
+    console.error(e);
+    res.status(401).json({ message: "Invalid token" });
+  }
 };
-
