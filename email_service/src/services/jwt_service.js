29ba7167f17c670bb76b25564d0c26faad6019d5
niiicolai <niiicolai@gmail.com>
2025-05-28 22:13:20 +0200

    update

---
+++ email_service/src/services/jwt_service.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+if (!JWT_SECRET) console.error("JWT_SECRET is not set in .env file");
+
+export default class JwtService {
+  /**
+   * @function verify
+   * @param {String} token
+   * @returns {Promise<Object>}
+   */
+  static verify(token) {
+    if (!token) throw new Error("Token is required");
+    if (typeof token !== "string") throw new Error("Token must be a string");
+
+    try {
+      return jwt.verify(token, JWT_SECRET);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
