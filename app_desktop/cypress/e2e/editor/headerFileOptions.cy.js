427c3fb7e5e31e14db965deaf2abd8f6981a8647
niiicolai <niiicolai@gmail.com>
2025-06-09 04:03:54 +0200

    update

--- app_desktop/cypress/e2e/editor/headerFileOptions.cy.js
+++ app_desktop/cypress/e2e/editor/headerFileOptions.cy.js
@@ -8,18 +8,4 @@ describe('editor header-file test', () => {
     cy.get('[data-testid=editor-header-new-file-button]').click();
     cy.get('[data-testid=editor-file-tabs]').children().should('have.length', 2);
   });
-
-  it('test header-file go to login success', () => {
-    cy.goto('/');
-    cy.get('#drop-down-button-header-file-drop-down').click();
-    cy.get('[data-testid=editor-header-login-button]').click();
-    cy.urlShould('/user/login');
-  });
-
-  it('test header-file go to signup success', () => {
-    cy.goto('/');
-    cy.get('#drop-down-button-header-file-drop-down').click();
-    cy.get('[data-testid=editor-header-signup-button]').click();
-    cy.urlShould('/user/signup');
-  });
 });
